fcac2d42b826906b9c53447cae320bdb9b94fd30
Fábio <fsmaiorano@gmail.com>
2018-03-27 18:08:04 -0300

    add: sass configuration

--- webpack.config.js
+++ webpack.config.js
@@ -15,12 +15,22 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/, 
+                test: /\.js$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                 }
             },
+            {
+                test: /\.scss$/,
+                use: [{
+                    loader: "style-loader"
+                }, {
+                    loader: "css-loader"
+                }, {
+                    loader: "sass-loader",
+                }]
+            }
         ]
     },
 }
